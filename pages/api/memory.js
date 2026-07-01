4b696befb6166ab5c1d9f1733f0655c3f688f607
hxj940107 <hxj9417@gmail.com>
2026-07-01 14:28:01 +0800

    Update memory.js

--- pages/api/memory.js
+++ pages/api/memory.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   "https://cncpymfqefyabiclnqzg.supabase.co",
-  "你的service_role_key"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuY3B5bWZxZWZ5YWJpY2xucXpnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4Mjg3MDM2NywiZXhwIjoyMDk4NDQ2MzY3fQ.g8m820mqDLnyPBt3O3o2tCEYH26X8TiTfQXsGeeq7jM"
 )
 
 export default async function handler(req, res) {
