5ce830f79f060176800557d6bb628ca5d7cdafca
hxj940107 <hxj9417@gmail.com>
2026-07-01 15:46:42 +0800

    Update memory.js

--- api/memory.js
+++ api/memory.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
   } catch (err) {
     return res.status(500).json({
-      error: err.message
+      error: err.message || String(err)
     })
   }
 }
